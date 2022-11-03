3ef86926485d6705f6375f637b85ccc861b86833
Rafael Almeida <eafaelsilva@gmail.com>
2022-11-03 09:48:26 -0300

    Add Grapic User Interface program .

---
+++ HelloWorldJava/GiuTeste.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JOptionPane;
+
+public class GiuTeste {
+
+    public static void main(String[] args)
+    {
+        String name = JOptionPane.showInputDialog("Enter Your Name");
+        JOptionPane.showMessageDialog(null, "Hello"+name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Your age"));
+
+        JOptionPane.showMessageDialog(null, "You are "+name+" years old "+age);   
+    }
+
+}
