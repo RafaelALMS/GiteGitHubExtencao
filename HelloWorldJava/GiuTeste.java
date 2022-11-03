ac42f24f55984e89041fe6efbf4ad595bf3825ee
Rafael Almeida <eafaelsilva@gmail.com>
2022-11-03 10:11:55 -0300

    a

--- HelloWorldJava/GiuTeste.java
+++ HelloWorldJava/GiuTeste.java
@@ -6,7 +6,7 @@ public class GiuTeste {
     public static void main(String[] args)
     {
         String name = JOptionPane.showInputDialog("Enter Your Name");
-        JOptionPane.showMessageDialog(null, "Hello"+name);
+        JOptionPane.showMessageDialog(null, "Hello "+name);
 
         int age = Integer.parseInt(JOptionPane.showInputDialog("Enter Your age"));
 
